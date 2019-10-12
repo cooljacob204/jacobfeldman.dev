209ee503edbd3d74c1eb41b168aee6411216b441
Jacob Feldman <jacobgfeld@gmail.com>
2019-10-12 17:12:00 -0400

    Fixes linkenin img alt tag

--- src/components/logos/Linkedin.jsx
+++ src/components/logos/Linkedin.jsx
@@ -4,7 +4,7 @@ export default function Linkedin(props) {
   return (
     <div style={{ ...props.style, display: 'inline-block' }}>
       <a href='https://www.linkedin.com/in/jacobgfeld/' target="_blank" rel="noopener noreferrer">
-        <img alt='github link' src={props.theme.logos.Linkedin.img} height={props.height} width={props.width}></img>
+        <img alt='Linkenin link' src={props.theme.logos.Linkedin.img} height={props.height} width={props.width}></img>
       </a>
     </div>
   )
