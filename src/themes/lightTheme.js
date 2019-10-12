6e9fd9f383b98e2219e5403b14d4663fed286e0b
Jacob Feldman <jacobgfeld@gmail.com>
2019-10-12 17:31:13 -0400

    adds a space

--- src/themes/lightTheme.js
+++ src/themes/lightTheme.js
@@ -1,7 +1,7 @@
 const LIGHT_THEME = {
   App: { body: { style: { backgroundColor: 'white' } }, div: { style:{} } },
   TopBar: { header: { style: { backgroundColor: '#f1f1f1' }, icon: { src: 'moon.png' } } },
-  logos:{
+  logos: {
     Github: { img: 'GitHub-Mark-120px-plus.png' },
     Linkedin: { img: 'linkedin-dark-128px.png'}
   }
