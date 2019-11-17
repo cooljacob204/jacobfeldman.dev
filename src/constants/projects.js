5ef0dfb079ad7e30b809e68fc9bea9a070c002a8
Jacob Feldman <me@jacobfeldman.dev>
2019-11-17 14:04:52 -0500

    Adds projects constant

---
+++ src/constants/projects.js
@@ -0,0 +1,8 @@
+const PROJECTS = [
+  { 
+    name: 'Unifi Api' ,
+    description: 'A gem for interacting with a Unifi controller',
+    url: null,
+    source: 'https://github.com/cooljacob204/unifi_api'
+  }
+]
