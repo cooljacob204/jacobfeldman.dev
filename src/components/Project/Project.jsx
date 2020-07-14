62fd3c3d4b5bf0a428b8d9f08b7fbd4eee5c5b11
Jacob Feldman <me@jacobfeldman.dev>
2020-07-14 13:13:16 -0400

    Fixs urls, adds another project

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -4,9 +4,9 @@ export default function Project({ project }) {
   return (
     <div className='project'>
       <h4>{project.name}</h4>
-      {project.url && <p>Website: {project.url}</p>}
-      <p>Description: {project.description}</p>
-      <p>Source: <a href={project.source}>{project.source}</a></p>
+      {project.url && <p><b>Website:</b> <a href={`https://${project.url}`} target='_blank'>{project.url}</a></p>}
+      <p><b>Description: </b>{project.description}</p>
+      <p><b>Source: </b><a href={`https://github.com/${project.source}`} target='_blank'>{project.source}</a></p>
     </div>
   )
 }
--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,15 +1,21 @@
 const PROJECTS = [
+  {
+    name: 'jacobfeldman.dev',
+    description: "My portfolio website (You're on it!)",
+    url: 'jacobfeldman.dev',
+    source: 'cooljacob204/jacobfeldman.dev'
+  },
   { 
     name: 'Unifi Api' ,
     description: 'A ruby gem api wrapper for the undocumented Unifi controller API',
     url: null,
-    source: 'github.com/cooljacob204/unifi_api'
+    source: 'cooljacob204/unifi_api'
   },
   {
     name: 'Videogame-Tracker',
     description: 'A website for tracking video games in your library',
-    url: 'videogame-tracker.jacobfeldman.dev',
-    source: 'github.com/cooljacob204/videogame-tracker'
+    url: 'videogame-tracker.lfp2.dev',
+    source: 'cooljacob204/videogame-tracker'
   }
 ]
 
