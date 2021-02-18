56e1c74ac5b61b4446a8f1c74dd6861dc1ff6ed4
Jacob Feldman <me@jacobfeldman.dev>
2021-02-18 11:49:50 -0500

    Adds github and linked icons/links

--- src/components/Me/Me.jsx
+++ src/components/Me/Me.jsx
@@ -26,6 +26,12 @@ export default function Me(){
       <img src='/centerpiece/background1.svg' alt='background 1' />
       <img src='/centerpiece/background2.svg' alt='background 2' />
       <img src='/centerpiece/JF.png' alt='initials' />
+      <a href='https://www.linkedin.com/in/jacobgfeld/' target='_blank'>
+        <img src='/centerpiece/linkedin-small-white.svg' alt='linkedin' />
+      </a>
+      <a href='https://github.com/cooljacob204' target='_blank'>
+        <img src='/centerpiece/github-small-white.svg' alt='github' />
+      </a>
       <img src='/centerpiece/me.png' alt='me' />
     </div>
   </div>
